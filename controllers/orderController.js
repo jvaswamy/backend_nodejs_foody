451bd417cb5eb0bf08455c3bc288aaea182853d1
jvaswamy <109134449+jvaswamy@users.noreply.github.com>
2026-06-03 21:38:48 +0530

    Update success and cancel URLs in orderController

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -47,8 +47,8 @@ const placeOrder = async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       line_items: line_items,
       mode: "payment",
-      success_url: `${baseFrontend}/#/verify?success=true&orderId=${newOrder._id}`,
-      cancel_url: `${baseFrontend}/#/verify?success=false&orderId=${newOrder._id}`,
+      success_url: `${baseFrontend}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${baseFrontend}/verify?success=false&orderId=${newOrder._id}`,
     });
 
     res.json({ success: true, session_url: session.url });
