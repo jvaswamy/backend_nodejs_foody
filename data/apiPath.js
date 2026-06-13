ea5bf3a5f7f6c5085d61096a4f2fea793326ff56
jvaswamy <109134449+jvaswamy@users.noreply.github.com>
2026-06-13 18:30:17 +0530

    Update apiPath.js

--- data/apiPath.js
+++ data/apiPath.js
@@ -1,3 +1,3 @@
-export const PORT = "https://foody-app-epre.onrender.com";
+export const PORT = "https://backend-nodejs-foody-plp8.onrender.com";
 export const FRONTEND_URL = "https://frontend-foody-gold.vercel.app";
 // export const FRONTEND_URL = "http://localhost:5173";
